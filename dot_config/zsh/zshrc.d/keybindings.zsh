b0b3b16a78ae96bea95d9cf01ade4c651809ce9d
Eric Brusseau <eric@brusseau.org>
2025-04-17 13:49:57 -0700

    Updated zsh configs

--- dot_config/zsh/zshrc.d/keybindings.zsh
+++ dot_config/zsh/zshrc.d/keybindings.zsh
@@ -1,3 +1,13 @@
 # ZSH Plugin: zsh-users/zsh-history-substring-search
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+# bindkey '^[[A' history-substring-search-up
+# bindkey '^[[B' history-substring-search-down
+[[ -v terminfo ]] || zmodload zsh/terminfo
+
+if [[ -n "$terminfo[kcuu1]" ]]; then
+  bindkey -M emacs "$terminfo[kcuu1]" history-substring-search-up
+  bindkey -M viins "$terminfo[kcuu1]" history-substring-search-up
+fi
+if [[ -n "$terminfo[kcud1]" ]]; then
+  bindkey -M emacs "$terminfo[kcud1]" history-substring-search-down
+  bindkey -M viins "$terminfo[kcud1]" history-substring-search-down
+fi
